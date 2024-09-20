2cb6be95d7436276f1c3c0956d87b0c6167ced93
ahmedShaheer3 <122429039+ahmedShaheer3@users.noreply.github.com>
2024-09-20 17:51:04 +0500

    d
    d

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -6,8 +6,8 @@ import { socketUserType } from "../types/appTypes";
  ** Registering to an event so can user can joing chat rooms
  */
 const mountJoinChatEvent = (socket: Socket) => {
-  socket.on(ChatEventEnum.JOIN_CHAT_EVENT, (chatId: string, userId: string) => {
-    console.log(`User joined the chat 🤝. chatId: `, chatId, userId);
+  socket.on(ChatEventEnum.JOIN_CHAT_EVENT, (chatId: string) => {
+    console.log(`User joined the chat 🤝. chatId: `, chatId);
     // joining the room with the chatId will allow specific events to be fired where we don't bother about the users like typing events
     // E.g. When user types we don't want to emit that event to specific participant.
     // We want to just emit that to the chat where the typing is happening
